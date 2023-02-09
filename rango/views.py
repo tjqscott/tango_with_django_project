832b940371d99aeac143d4614b879a01456f35b7
tjqscott <50828657+tjqscott@users.noreply.github.com>
2023-02-09 00:59:49 +0000

    Chapter 3 complete

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def index(request):
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	return render(request, 'rango/index.html', context=context_dict)
+def about(request):
+	context_dict = {'yourname': 'Taylor Scott'}
+	return render(request, 'rango/about.html', context=context_dict)
